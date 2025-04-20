929c5c3a8626c9a1d2477f5b871c8f65ee5d4e9c
Lucas Lima <lucasgabrieljake@gmail.com>
2025-04-20 11:07:07 -0300

    shift rows

--- S_AES.py
+++ S_AES.py
@@ -8,18 +8,18 @@ class S_AES():
         
         self._k0, self._k1, self._k2 =  self._expand_key(key)
 
-    def _substitute_nibbles(self, value: np.uint8) -> np.uint8:
+    def _substitute_nibbles_in_key_expansion(self, value: np.uint8) -> np.uint8:
             N0 = value & 0b1111
             N1 = (value >> 4) & 0b1111
 
             return ((self._S_box[N1] << 4) + self._S_box[N0])
     
-    def _rotate_niblles(self, value: np.uint8) -> np.uint8:
+    def _rotate_nibbles(self, value: np.uint8) -> np.uint8:
         return ((value >> 4) & 0xF) + ((value << 4) & 0xFF)
 
     def _G(self, value: np.uint8, round: int) -> np.uint8:
         RCON = [0, 0b10000000, 0b00110000]
-        return RCON[round] ^ self._substitute_nibbles(self._rotate_niblles(value))
+        return RCON[round] ^ self._substitute_nibbles_in_key_expansion(self._rotate_nibbles(value))
     
     def _expand_key(self, key: np.uint16) -> tuple[np.uint16]:
         build_key = lambda x0, x1: (x0<<8) + x1  
@@ -41,10 +41,28 @@ class S_AES():
 
         return (K0, K1, K2)
 
-    def _add_round_key(data: np.uint16, key: np.uint16):
+    def _add_round_key(self, data: np.uint16, key: np.uint16):
         return data ^ key 
 
+    def _substitute_nibbles(self, value: np.int16) -> np.int16:
+        shift = 0
+        new_value = 0
+        for _ in range(4):
+            current_nibble = value & 0xF
+            current_nibble = self._S_box[current_nibble]
+            new_value += (current_nibble << shift)
+            value >>= 4
+            shift += 4 
+        return new_value
+    
+    def _shift_rows(self, value: np.int16) -> np.int16:
+        c0, c1 = (value >> 8), (value & 0xFF)
+        m01, m11  = c0 & 0xF, c1 & 0xF
 
+        c0 = c0 ^ m01 ^ m11 
+        c1 = c1 ^ m01 ^ m11 
+        
+        return (c0 << 8) + c1
 
 
 
